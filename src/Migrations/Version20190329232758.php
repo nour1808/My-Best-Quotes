1b3c6a56027e0ddeaafc827e479b0677e55010ad
Noureddine Berjaoui <nour1808@gmail.com>
2019-03-30 13:49:38 +0100

    add quote

--- src/Migrations/Version20190329232758.php
+++ src/Migrations/Version20190329232758.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190328085803 extends AbstractMigration
+final class Version20190329232758 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,7 @@ final class Version20190328085803 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         //$this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        //$this->addSql('ALTER TABLE user ADD reset_password VARCHAR(255) DEFAULT NULL');
+        //$this->addSql('ALTER TABLE user ADD reset_password VARCHAR(255) DEFAULT NULL, CHANGE updated_at updated_at DATETIME NOT NULL, CHANGE is_actif is_actif TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +30,6 @@ final class Version20190328085803 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user DROP reset_password');
+        $this->addSql('ALTER TABLE user DROP reset_password, CHANGE updated_at updated_at DATETIME DEFAULT NULL, CHANGE is_actif is_actif TINYINT(1) DEFAULT NULL');
     }
 }
